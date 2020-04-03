fe1d0b9a4401404965cd07aba537914d88c6f1f7
Colan Biemer <colan@comcast.net>
2020-04-03 18:09:56 -0400

    added the ability to get ids and tokens from the id container for n-grams

--- AI/NGram/Utility/NGramIDContainer.cs
+++ AI/NGram/Utility/NGramIDContainer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using UnityEngine.Assertions;
 using Tools.ID;
 
 namespace Tools.AI.NGram.Utility
@@ -9,11 +10,44 @@ namespace Tools.AI.NGram.Utility
         private Dictionary<string, string> idToColumn = null;
         private IEnumerator<string> idGenerator = null;
 
-        public NGramIDContainer(int idSize = 15)
+        public NGramIDContainer(int idSize = 10)
         {
             columnToId = new Dictionary<string, string>();
             idToColumn = new Dictionary<string, string>();
             idGenerator = StringIDGenerator.GetID(idSize).GetEnumerator();
+        }
+
+        public string GetID(string token)
+        {
+            if (columnToId.ContainsKey(token) == false)
+            {
+                idGenerator.MoveNext();
+                string id = idGenerator.Current;
+
+                columnToId.Add(token, id);
+                idToColumn.Add(id, token);
+
+                return id;
+            }
+
+            return columnToId[token];
+        }
+
+        public List<string> GetIDs(IEnumerable<string> tokens)
+        {
+            List<string> ids = new List<string>();
+            foreach (string col in tokens)
+            {
+                ids.Add(GetID(col));
+            }
+
+            return ids;
+        }
+
+        public string GetToken(string id)
+        {
+            Assert.IsTrue(idToColumn.ContainsKey(id));
+            return idToColumn[id];
         }
     }
 }
