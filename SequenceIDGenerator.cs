c204a822763a1878a3545e2a5a552f1776ee6441
Colan Biemer <colan@comcast.net>
2020-04-02 23:07:25 -0400

    id generators

---
+++ SequenceIDGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace UnityUtility
+{
+    /// <summary>
+    /// Simple class to generate integers from 0 to max int.
+    /// </summary>
+    public class SequenceIDGenerator
+    {
+        private int id = 0;
+
+        public int NextID()
+        {
+            return ++id;
+        }
+    }
+}
---
+++ StringIDGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.Assertions;
+using System.Collections.Generic;
+
+namespace UnityUtility
+{
+    public static class StringIDGenerator
+    {
+        private const string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public static IEnumerable<string> GetID(int size)
+        {
+            Assert.IsTrue(size > 0);
+            for (int i = 0; i < characters.Length; ++i)
+            {
+                if (size == 1)
+                {
+                    yield return characters[i].ToString();
+                }
+                else
+                {
+                    string c = characters[i].ToString();
+                    foreach (string id in GetID(size - 1))
+                    {
+                        yield return $"{c}{id}";
+                    }
+                }
+            }
+        }
+    }
+}
