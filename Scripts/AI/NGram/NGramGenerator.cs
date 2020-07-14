3e75655d51636e11add57cecf693ca17f4bd8033
Colan Biemer <colan@comcast.net>
2020-07-14 15:27:56 -0400

    ngram generator now provides option to include a classifier

--- Scripts/AI/NGram/NGramGenerator.cs
+++ Scripts/AI/NGram/NGramGenerator.cs
@@ -1,59 +1,93 @@
 ﻿using System.Collections.Generic;
-using UnityEngine.Assertions;
 using Tools.DataStructures;
 using System;
+using UnityEngine.Assertions;
 
 namespace Tools.AI.NGram
 {
     public static class NGramGenerator
     {
         /// <summary>
-        /// This will generate a list of output where it assumes that there is a
-        /// final state that can be reached. Like how a mario level always ends 
-        /// with a flag. If there is no final state this will be slow and fail.
+        /// This is built around the idea that you can take input for an n-gram
+        /// and simplify it into categories. Obviously this function cannot 
+        /// implement a generic classifier so it received a lambda which from 
+        /// the user which handles the implementation specific details. This
+        /// generator will generate for that size output that only matches the
+        /// specific type specified by accepted type. 
+        /// 
+        /// A possible extension would beo make acceptedType a list so that a user
+        /// can say that any of the following types are acceptable.
         /// </summary>
-        /// <param name="gram"></param>
+        /// <param name="grammar"></param>
         /// <param name="startInput"></param>
-        /// <param name="minSize"></param>
-        /// <param name="maxSize"></param>
+        /// <param name="size"></param>
+        /// <param name="acceptedType"></param>
+        /// <param name="classifier"></param>
         /// <returns></returns>
-        public static List<string> Generate(ICompiledGram gram, List<string> startInput, int size)
+        public static List<string> GenerateRestricted(
+            ICompiledGram grammar,
+            List<string> startInput,
+            int size,
+            string acceptedType,
+            Func<string, string> classifier)
         {
-            CircularQueue<string> queue = new CircularQueue<string>(gram.GetN() - 1);
+            Assert.IsFalse(string.IsNullOrEmpty(acceptedType));
+            Assert.IsNotNull(classifier);
+            Assert.IsNotNull(grammar);
+            Assert.IsTrue(size > 0);
+            Assert.IsNotNull(startInput);
+            Assert.IsTrue(startInput.Count > grammar.GetN() - 1);
+
+            CircularQueue<string> queue = new CircularQueue<string>(grammar.GetN() - 1);
             queue.AddRange(startInput);
 
-            List<string> outputLevel = GenerateTree(gram, queue, size); 
+            List<string> outputLevel = GenerateTree(grammar, queue, size, acceptedType, classifier);
             List<string> level = new List<string>(startInput);
             level.AddRange(outputLevel);
 
             return level;
         }
 
-        private static List<string> GenerateTree(ICompiledGram gram, CircularQueue<string> prior, int size)
+        /// <summary>
+        /// This will generate a list of output where it assumes that there is a
+        /// final state that can be reached. Like how a mario level always ends 
+        /// with a flag. If there is no final state this will be slow and fail.
+        /// </summary>
+        /// <param name="grammar"></param>
+        /// <param name="startInput"></param>
+        /// <param name="minSize"></param>
+        /// <param name="maxSize"></param>
+        /// <returns></returns>
+        public static List<string> Generate(
+            ICompiledGram grammar, 
+            List<string> startInput, 
+            int size)
+        {
+            CircularQueue<string> queue = new CircularQueue<string>(grammar.GetN() - 1);
+            queue.AddRange(startInput);
+
+            List<string> outputLevel = GenerateTree(grammar, queue, size, null, null); 
+            List<string> level = new List<string>(startInput);
+            level.AddRange(outputLevel);
+
+            return level;
+        }
+
+        private static List<string> GenerateTree(
+            ICompiledGram gram, 
+            CircularQueue<string> prior, 
+            int size,
+            string acceptedType,
+            Func<string, string> classifier)
         {
             string[] guesses = gram.GetGuesses(prior.ToArray());
-            //string[] newPrior = new string[prior.Length];
-
-            //// shift the prior to the left by one.
-            //for (int i = 0; i < prior.Length - 1; ++i)
-            //{
-            //    newPrior[i] = prior[i + 1];
-            //}
-
             foreach (string guess in guesses)
             {
-                // The current problem is that a flag is included in the unigram of
-                // the hierarchical n-gram. I'm not sure how to get around this 
-                // problem at the moment because the result is that a level can be
-                // generated with multiple endings when I really only want the level
-                // to be generated at the end. The other problem is that i will soon
-                // have the simplified level already generated. SO maybe I just remove
-                // them an auto place the flag at the end. Then I don't need this tree
-                // structure at all. Or the attempts. So I just generate of arbitrary
-                // length and then put a flag in at the end and call it a day.
-                //
-                // But with the simplified, I really do only want to generate pieces
-                // that correctly fit. with this method.
+                if (classifier != null && !classifier(guess).Equals(acceptedType))
+                {
+                    continue;
+                }
+
                 CircularQueue<string> newPrior = prior.Clone();
                 newPrior.Add(guess);
 
@@ -64,8 +98,7 @@ namespace Tools.AI.NGram
                 }
                 else if (gram.HasNextStep(newPrior.ToArray()))
                 {
-
-                    List<string> returnVal = GenerateTree(gram, newPrior, size - 1);
+                    List<string> returnVal = GenerateTree(gram, newPrior, size - 1, acceptedType, classifier);
                     if (returnVal != null)
                     {
                         returnVal.Insert(0, guess);
